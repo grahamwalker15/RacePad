fee1f6eecd69ed625b71955a6578439bb3785c3c
Gareth <Gareth@9925204c-63e1-4f11-ae18-372a25b3a23a>
2011-02-07 16:21:20 +0000

    Changed baseURL in help view controllers
    git-svn-id: https://sbgracing.sourcerepo.com/sbgracing/RacePad/RacePad@142 9925204c-63e1-4f11-ae18-372a25b3a23a

--- Classes/HelpViewControllers/TimingHelpController.m
+++ Classes/HelpViewControllers/TimingHelpController.m
@@ -22,38 +22,65 @@
 	switch(index)
 	{
 		case 0:
-			[html appendString:@"Welcome to RacePad Help."];
-			[html appendString:@"<p>Tap on any red question mark buttons to get specific information about that area of the screen."];
+			[html appendString:@"<h3>Timing</h3>"];
+			[html appendString:@"On the timing screen, you will find the positions of all drivers in the race or session, "];
+			[html appendString:@"together with all official timing information."];
+			
+			[html appendString:@"<p>You can also see details of every lap covered by each driver."];
+			
+			[html appendString:@"<p><table>"];
+			[html appendString:@"<td width = 20><image src = ""HelpButtonHTML.png""></td>"];
+			[html appendString:@"<td width = 10>&nbsp</td>"];
+			[html appendString:@"<td>Press the red question mark buttons to get more information about each area of the screen.<td>"];
+			[html appendString:@"<p></table>"];
 			break;
 			
 		case 1:
-			[html appendString:@"The left hand columns show drivers in current race or session order."];
-			[html appendString:@"<p>The numbers are their position, followed by their car number."];
-			[html appendString:@"<p>Abbreviations are used for driver names. These are usually the first three letters of a driver's surname. "];
+			[html appendString:@"These columns show drivers in current race or session order."];
+			[html appendString:@"<p>On the left is their helmet design followed by their position, car number "];
+			[html appendString:@"and abbreviated name."]; 
+			[html appendString:@"<p>The abbreviation is usually the first three letters of a driver's surname. "];
 			[html appendString:@"A full list of abbreviations can be found in the Info screen."];
+			[html appendString:@"<p>Double tab on a driver's name to see a full list of all of his lap times."];
 			break;
 			
 		case 2:
-			[html appendString:@"These columns show the type of tyre that the driver is currently using, together with the number of laps covered on those tyres."];
-			[html appendString:@"<p>The tyres may be Option (the softer dry tyre), Prime (the harder dry), Intermediate (for light rain) or Wet (for heavy rain)."];
+			[html appendString:@"The tyre columns show both the type of tyre that the driver is currently using, and the number of laps covered on those tyres."];
+			[html appendString:@"<p>The tyre type may be ""Option"" (the softer dry tyre), ""Prime"" (the harder dry), ""Inter"" (intermediate, for light rain) or ""Wet"" (for heavy rain)."];
 			[html appendString:@"<p>In a dry race, drivers must use both Option and Prime tyres. "];
-			[html appendString:@"In a wet race, it is not compulsory to change tyres."];
-			  
+			[html appendString:@"<p>In a wet race, it is not compulsory to change tyres."];
 			break;
 			
 		case 3:
-			[html appendString:@"<h3>Page 3</h3>"];
-			[html appendString:@"<p>Racepad Help"];
+			[html appendString:@"During the race, the lap time column shows the time for the last lap completed for the driver."];				
+			[html appendString:@"<br>During practice and qualifying, the time for the last completed lap is shown while "];
+			[html appendString:@"the driver is out on track. When he is in the pits, it will show the driver's best "];
+			[html appendString:@"time achieved in the session so far."];
+			[html appendString:@"<p>All lap and sector times are colour coded:"];
+			[html appendString:@"<br><font color = #DD00DD>Purple</font> indicates the best overall time of the session so far"];
+			[html appendString:@"<br><font color = #00DD00>Green</font> indicates the driver's personal best time of the session so far"];
+			[html appendString:@"<br><font color = #DD0000>Red</font> indicates an in or out lap"];
+			[html appendString:@"<br>White is the normal colour otherwise"];
 			break;
 			
 		case 4:
-			[html appendString:@"<h3>Page 4</h3>"];
-			[html appendString:@"<p>Racepad Help"];
+			[html appendString:@"The Gap column shows the gap from this driver to the leader."];
+			[html appendString:@"<br>The Interval column shows the gap to the driver ahead."];
+			[html appendString:@"<p>In the race, these are the time gaps on track at the end of the previous lap."];
+			[html appendString:@"<br>In practice and qualifying, the gaps between the best lap times of the drivers are shown."];
+			[html appendString:@"<p>In the race, the current lap number for the leader is shown against his name, in the gap column."];
 			break;
 			
 		case 5:
-			[html appendString:@"<h3>Page 5</h3>"];
-			[html appendString:@"<p>Racepad Help"];
+			[html appendString:@"The track is divided into three official sections, called sectors. These columns show "];
+			[html appendString:@"the times for the driver for each of these sectors, together with the speed in "];
+			[html appendString:@"kilometers per hour at the end of each."];
+			[html appendString:@"<p>No data is shown for a sector until the driver has completed that sector on his current lap."];
+			[html appendString:@"<p>The times are colour coded as follows:"];
+			[html appendString:@"<br><font color = #DD00DD>Purple</font> indicates the best overall time of the session so far"];
+			[html appendString:@"<br><font color = #00DD00>Green</font> indicates the driver's personal best time of the session so far"];
+			[html appendString:@"<br><font color = #DD0000>Red</font> indicates an in or out lap"];
+			[html appendString:@"<br>White is the normal colour otherwise"];
 			break;
 			
 		case 6:
@@ -84,7 +111,7 @@
 	
 	[html appendString:@"</body</html>"];
 	
-	[webView loadHTMLString:html baseURL:nil];
+	[webView loadHTMLString:html baseURL:[[NSBundle mainBundle] bundleURL]];
 	
 	[html release];	
 }
