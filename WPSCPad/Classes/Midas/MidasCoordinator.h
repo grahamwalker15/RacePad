4b93cc3b49263aadc217435b530506a8edba14c4
Gareth <Gareth@9925204c-63e1-4f11-ae18-372a25b3a23a>
2012-01-04 12:45:21 +0000

    Added Midas specific coordinator and home view
    git-svn-id: https://sbgracing.sourcerepo.com/sbgracing/RacePad/Branches@272 9925204c-63e1-4f11-ae18-372a25b3a23a

---
+++ WPSCPad/Classes/Midas/MidasCoordinator.h
@@ -0,0 +1,19 @@
+//
+//  MidasCoordinator.h
+//  MidasDemo
+//
+//  Created by Gareth Griffith on 1/4/12.
+//  Copyright 2012 SBG Racing Services Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "RacePadCoordinator.h"
+
+
+@interface MidasCoordinator : RacePadCoordinator
+{
+
+}
+
+@end
---
+++ WPSCPad/Classes/Midas/MidasCoordinator.m
@@ -0,0 +1,14 @@
+//
+//  MidasCoordinator.m
+//  MidasDemo
+//
+//  Created by Gareth Griffith on 1/4/12.
+//  Copyright 2012 SBG Racing Services Ltd. All rights reserved.
+//
+
+#import "MidasCoordinator.h"
+
+
+@implementation MidasCoordinator
+
+@end
