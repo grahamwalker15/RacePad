f0a7d562fbaea4d27e13d8168f4b82604f998162
Daniel <Daniel@9925204c-63e1-4f11-ae18-372a25b3a23a>
2012-09-07 11:05:38 +0000

    Add web view controller
    git-svn-id: https://sbgracing.sourcerepo.com/sbgracing/RacePad/Branches@474 9925204c-63e1-4f11-ae18-372a25b3a23a

---
+++ WPSCPad/Classes/Midas Store/MidasWebViewController.h
@@ -0,0 +1,15 @@
+//
+//  MidasWebViewController.h
+//  MidasDemo
+//
+//  Created by Daniel Tull on 07/09/2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MidasWebViewController : UIViewController
+
+- (id)initWithURL:(NSURL *)URL;
+
+@end
---
+++ WPSCPad/Classes/Midas Store/MidasWebViewController.m
@@ -0,0 +1,84 @@
+//
+//  MidasWebViewController.m
+//  MidasDemo
+//
+//  Created by Daniel Tull on 07/09/2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import "MidasWebViewController.h"
+
+@interface MidasWebViewController ()
+@property (nonatomic, weak) IBOutlet UINavigationBar *navigationBar;
+@property (nonatomic, weak) IBOutlet UIWebView *webView;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *backBarButtonItem;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *forwardBarButtonItem;
+@end
+
+@implementation MidasWebViewController {
+	__strong NSURL *_URL;
+}
+
+#pragma mark - NSObject
+
+- (id)init {
+	return [self initWithNibName:@"MidasWebViewController" bundle:nil];
+}
+
+#pragma mark - UIViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	[self.navigationBar setItems:@[self.navigationItem] animated:NO];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+	[super viewWillAppear:animated];
+	[self.webView loadRequest:[NSURLRequest requestWithURL:_URL]];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+	return YES;
+}
+
+#pragma mark - MidasWebViewController
+
+- (id)initWithURL:(NSURL *)URL {
+	self = [self init];
+	if (!self) return nil;
+	_URL = [URL copy];
+	_backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStyleBordered target:self action:@selector(webViewBack:)];
+	_forwardBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Forward" style:UIBarButtonItemStyleBordered target:self action:@selector(webViewForward:)];
+	self.navigationItem.leftBarButtonItems = @[ _backBarButtonItem, _forwardBarButtonItem ];
+	return self;
+}
+
+- (IBAction)webViewBack:(id)sender {
+	[self.webView goBack];
+}
+
+- (IBAction)webViewForward:(id)sender {
+	[self.webView goForward];
+}
+
+- (void)_updateButtonStates {
+	self.title = [self.webView stringByEvaluatingJavaScriptFromString:@"document.title"];
+	self.backBarButtonItem.enabled = self.webView.canGoBack;
+	self.forwardBarButtonItem.enabled = self.webView.canGoForward;
+}
+
+#pragma mark - UIWebViewDelegate
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+	[self _updateButtonStates];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+	[self _updateButtonStates];
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+	[self _updateButtonStates];
+}
+
+@end
